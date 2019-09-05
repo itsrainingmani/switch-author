e289de3725fe4cc7fc69bc0047cea266605a2df3
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-09-05 17:40:32 -0400

    Parse args for name and email and print it out

--- src/bin.js
+++ src/bin.js
@@ -1,8 +1,21 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 import chalk from 'chalk';
 import mri from 'mri';
 
 process.stdout.write(chalk.green('Welcome to the switch-author tool\n'));
-const args = mri(process.argv.slice(2));
-process.stdout.write(`Args: ${args}`);
+const args = mri(process.argv.slice(2), {
+	alias: {
+		n: 'name',
+		e: 'email',
+		d: 'depth'
+	}
+});
+
+const [name, email] = [args.name, args.email];
+
+if (args.help) {
+	process.stdout.write(`\nHELP STRING INCOMING\n`);
+} else {
+	process.stdout.write(`New name: ${name}\nNew email: ${email}`);
+}
